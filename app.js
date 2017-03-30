ca3a849ce0f528f375fe034d5352386c5794c5a7
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-03-30 10:25:45 -0700

    members index, show, delete included

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 var founders = require('./founders.js')
 var houses = require('./houses.js')
-// var members = require('./members.js')
+var members = require('./members.js')
 
 app.get('/founders', getFounders);
 app.get('/founders/:index', getFounderIndex, founderIndexShow);
@@ -10,8 +10,10 @@ app.delete('/founders/:index', getFounderIndex, founderIndexDelete);
 app.get('/houses', getHouses);
 app.get('/houses/:index', getHouseIndex, houseIndexShow);
 app.delete('/houses/:index', getHouseIndex, houseIndexDelete);
-// app.get('/members', getMembers)
-// app.get('/members/:index', getMemberOne)
+app.get('/members', getMembers)
+app.get('/members/:index', getMemberIndex, memberIndexShow);
+app.delete('/members/:index', getMemberIndex, memberIndexDelete);
+
 app.listen(3000, listenHandler)
 
 function getFounders(req, res) {
@@ -54,6 +56,30 @@ function houseIndexDelete(req, res) {
   houses.splice(req.params.index, 1);
   res.json(req.house);
 }
+
+//////////////////////////////////////////
+
+function getMembers(req, res) {
+  res.json(members);
+}
+
+function getMemberIndex(req, res, next) {
+  var index = req.params.index;
+  var member = members[index];
+  req.member = member;
+  next();
+}
+
+function memberIndexShow(req, res) {
+  res.json(req.member);
+}
+
+function memberIndexDelete(req, res) {
+  members.splice(req.params.index, 1);
+  res.json(req.member);
+}
+
+///////////////////////////
 
 function listenHandler() {
   console.log(`Listening on localhost:3000`);
